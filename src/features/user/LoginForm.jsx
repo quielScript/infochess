df8253a31598a4ab77b10e0238a6d35764914dad
quielLovesLasagna <exequielarco23@gmail.com>
2025-01-11 13:11:00 +0800

    Redirect from login to app

--- src/features/user/LoginForm.jsx
+++ src/features/user/LoginForm.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function LoginForm() {
+	const [username, setUSername] = useState("");
+	const navigate = useNavigate();
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (username.trim() !== "") {
+			navigate("/app");
+		}
+	}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className="flex flex-col items-start space-y-2">
 				<label htmlFor="username" className="text-transparentWhite">
 					Proceed by entering your name
 				</label>
 				<input
+					required
 					type="text"
 					id="username"
 					name="username"
 					className="bg-transparent border-[1px] rounded-md border-transparentWhite py-2 px-4 w-full text-transparentWhite"
 					placeholder="Enter name"
+					value={username}
+					onChange={(e) => setUSername(e.target.value)}
 				/>
-				<button className="px-8 py-2 text-base bg-white rounded-md text-warmBlack">
+				<button
+					type="submit"
+					className="px-8 py-2 text-base bg-white rounded-md text-warmBlack"
+				>
 					Enter
 				</button>
 			</div>
