3b9ed425f34dd8d7cbdb74dd65ee860a1e5df5e3
quielLovesLasagna <exequielarco23@gmail.com>
2025-01-11 12:27:21 +0800

    Images, font-family, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ["Poppins", "sans-serif"],
+		},
+		extend: {
+			colors: {
+				warmBlack: "#312E2B", // Page BG
+				smokyBlack: "#272522", // Card BG
+				lightGray: "#D9D9D9", // Header TXT
+				transparentWhite: "#ffffffd9", // Normal TXT
+				brightCyan: "#009FD9", // Link
+				oliveGreen: "#81b64c", // State
+			},
+		},
 	},
 	plugins: [],
 };
