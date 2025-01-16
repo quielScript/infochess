9cfdd6f1925aefd7bae704af96de5af6238b0d1e
quielLovesLasagna <exequielarco23@gmail.com>
2025-01-16 20:24:06 +0800

    Cleaning unnecessary comments

--- src/services/apiChess.js
+++ src/services/apiChess.js
@@ -1,12 +1,5 @@
 "use strict";
 
-// const PLAYER_SEARCH_API_URL = "https://api.chess.com/pub/player/{username}";
-// const TITLED_PLAYERS_API_URL = "https://api.chess.com/pub/titled/{title}";
-// const PLAYER_STATS_API_URL =
-// 	"https://api.chess.com/pub/player/{username}/stats";
-// const LEADERBOARDS_API_URL = "https://api.chess.com/pub/leaderboards";
-// const STREAMERS_API_URL = "https://api.chess.com/pub/streamers";
-
 export async function searchPlayer(username) {
 	try {
 		const res = await fetch(`https://api.chess.com/pub/player/${username}`);
